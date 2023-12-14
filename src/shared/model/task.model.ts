6c41e6b03b4af7092eccca05200127fc690403c1
Monika Jarosz <monikaj@qualtrics.com>
2023-12-14 13:57:09 +0100

    add Task model

---
+++ src/shared/model/task.model.ts
@@ -0,0 +1,28 @@
+export enum TaskStatus {
+	TODO = 'To do',
+	IN_PROGRESS = 'In progress',
+	DONE = 'Done',
+	CANCELED = 'Canceled'
+}
+
+export enum TaskPriority {
+	HIGH = 'High',
+	MEDIUM = 'Medium',
+	LOW = 'Low'
+}
+
+export enum TaskSize {
+	XS = 'XS',
+	S = 'S',
+	M = 'M',
+	L = 'L',
+	XL = 'XL',
+}
+
+export interface Task {
+	status: string;
+	name: string;
+	priority: string;
+	size: string;
+	details: string;
+}
