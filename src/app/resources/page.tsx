194f31588cc30b3d4dd9062364df9821b906e281
Monika Jarosz <monikaj@qualtrics.com>
2023-12-14 12:34:20 +0100

    add content to resources component

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -3,8 +3,17 @@ import './resources.css';
 
 export default function Resources() {
 	return (
-		<section>
-			Resources
-		</section>
+		<div id='MyResources' className='my-resources-container'>
+			<div className='sidebar'>
+			</div>
+
+			<div className='header'>
+				<h2>Welcome to your Personal Organizer!</h2>
+			</div>
+
+			<div className='main-content'>
+				<h3>My resources</h3>
+			</div>
+		</div>
 	)
 }
